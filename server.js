77d8d05cadf08025a8aee0760c93813f0dffd360
Jonathon Moore <jonmoore510@gmail.com>
2018-04-21 15:36:10 -0500

    Set new structure

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 // Dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const logger = require('morgan');
+const routes = require('./routes');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Express configuration
-const app = express();
-
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('client/build'));
+app.use(routes);
 
-app.use(express.static('public'));
+// MongoDB configuration
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/clannomnom');
 
-// MongoDB connection
-mongoose.connect('mongodb://localhost/clannom');
-const db = mongoose.connection;
-
+const db = mongoose.connection();
 db.on('error', error => console.log('Database Error: ', error));
+db.on('open', () => console.log('~~~ MongoDB connection successful ~~~'));
 
 // Listener
-app.listen(3001, () => console.log("Listening on port 3001!"));
+app.listen(PORT, () => console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`));
