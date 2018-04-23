a7b24175972346d7ff720d2c479e65ba4db9c318
Jonathon Moore <jonmoore510@gmail.com>
2018-04-23 16:45:10 -0500

    Changes to models

--- models/recipe.js
+++ models/recipe.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const recipeSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  ingredients: { type: String, required: true},
-  instructions: { type: String, required: true},
+  ingredients: { type: String, required: true },
+  instructions: { type: String, required: true },
   prep_time: { type: String, required: false },
   cook_time: { type: String, required: false},
   date: { type: Date, default: Date.now }
