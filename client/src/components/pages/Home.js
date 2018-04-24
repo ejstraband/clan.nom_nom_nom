2fa3a8759a48e1f1aa7e50ca456bc74829fd41d0
Craig Christensen <ruffcorn@gmail.com>
2018-04-23 19:29:22 -0500

    Backend React working

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => (
+    <div className="container col-4">
+        <h2>
+            This is the Main Page
+        </h2>
+    </div>
+);
+
+export default Home;
