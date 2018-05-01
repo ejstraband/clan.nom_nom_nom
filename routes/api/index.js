c0a5c953635e27c941aa48399b67d3171b8f6319
Jonathon Moore <jonmoore510@gmail.com>
2018-05-01 14:56:57 -0500

    Changed comment about books

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,7 @@ const recipeRoutes = require("./recipes");
 const userRoutes = require("./users");
 const emailRoute = require("./findByEmail");
 
-// Book routes
+// API routes
 router.use("/api/recipes", recipeRoutes);
 router.use("/api/users", userRoutes);
 router.use("/api/findByEmail", emailRoute);
