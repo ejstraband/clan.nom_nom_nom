ca924624a0a157411f08e563d017ae30228ddb5a
Jonathon Moore <jonmoore510@gmail.com>
2018-05-06 16:24:13 -0500

    Fixed the route I broke right after making it

--- routes/search/index.js
+++ routes/search/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userSearch = require("./userSearch");
+//const recentRecipeSearch = require("./recentRecipeSearch");
 
-router.use("/search", userSearch);
+router.use("/search/users/", userSearch);
+//router.use("/search/mostRecentRecipes", recentRecipeSearch);
 
 module.exports = router;
