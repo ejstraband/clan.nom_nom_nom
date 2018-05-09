d15f6066c7e77861797976ff4a72ac4b0e72560b
Craig Christensen <ruffcorn@gmail.com>
2018-05-09 13:26:26 -0500

    recipe card formatting

--- client/src/components/RecipeCard/RecipeCard.js
+++ client/src/components/RecipeCard/RecipeCard.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import Row from "../Row";
+import Col from "../Col";
 import "./index.css";
 import heartEmpty from "./heart-empty";
 import heartFull from "./heart-full";
@@ -18,22 +20,45 @@ class RecipeCard extends React.Component {
     user_favorites: [],
     recipe_id: "",
     author_id: "",
+    author_name: "",
+    poster_name: "",
     rating: 0 
   };
   
   componentDidMount(){
     let recipe = this.props.recipe;
+    // console.log("props:recipe is ",this.props.recipe)
     
     this.setState({
       recipe_id: recipe._id,
       rating: recipe.rating,
       author_id: recipe.author,      
     });
-
+    // get user favorites
     API.getUser(this.state.user_id)
     .then(res => {
-      this.setState({user_favorites: res.data.favorites});
+      this.setState({
+        user_favorites: res.data.favorites
+      });
       // console.log("favorites in state is ", this.state.user_favorites);
+    })
+    .catch(err => console.log("recipe card says, user find " + err));
+    // get author name
+    API.getUser(this.props.recipe.author)
+    .then(res => {      
+      this.setState({
+        author_name: res.data.name
+      });
+      // console.log("author name in state is ", this.state.author_name);
+    })
+    .catch(err => console.log("recipe card says, author find " + err));
+    // get poster name
+    API.getUser(this.props.recipe.poster)
+    .then(res => {
+      this.setState({
+        poster_name: res.data.name
+      });
+      // console.log("poster name in state is ", this.state.poster_name);
     })
     .catch(err => console.log("recipe card says, user find " + err));
   }
@@ -68,6 +93,10 @@ class RecipeCard extends React.Component {
     });
   };
 
+  getUserName = (id) => {
+
+  }
+
   render() {
     const { props } = this;
     const { recipe } = props;
@@ -77,12 +106,32 @@ class RecipeCard extends React.Component {
     console.log("recipe is: ", recipe);
     return (
       <div className="RecipeCard">
-        {this.renderTitle()}
-        <div className="RecipeCard__description">
-          {recipe.short_desc || "Lorem"}
-        </div>
-        <img style={{ width: "100%" }} src={recipe.image} alt="" />
-        {recipe.story}
+        <Row className="titleRow">
+          {this.renderTitle()}
+        </Row>
+        <Row className="userInfoRow">
+          <Col size="md-12">Author: { this.state.author_name }</Col>
+          <Col size="md-12">Posted by: { this.state.poster_name }</Col>
+        </Row>
+        <Row className="descriptionRow">
+          <Col size="md-1"></Col>
+          <Col size="md-11">
+            <div className="RecipeCard__description">
+              {recipe.short_desc}
+            </div>
+          </Col>
+        </Row>
+        <Row className="imageRow" style={{ paddingTop: 50, paddingBottom: 40, paddingLeft: 60, paddingRight: 60  }}>
+          <img className="recipeCard_Image" style={{ width: "100%" }} src={recipe.image} alt="" />
+        </Row>
+        <Row className="storyRow">
+          <Col size="md-1"></Col>
+          <Col size="md-10">
+            <div className="RecipeCard__story">
+              {recipe.story}
+            </div>
+          </Col>
+        </Row>
         <div style={{ flex: 1 }} />
         <div className="RecipeCard__buttons">
           {/* Show/hide recipe toggle */}
@@ -108,9 +157,7 @@ class RecipeCard extends React.Component {
           </button>
 
         </div>
-
         {this.renderMore()}
-
       </div>
     );
   }
@@ -120,10 +167,14 @@ class RecipeCard extends React.Component {
       return null;
     }
 
+    // let ingredients = this.props.recipe.ingredients.split(", ")
+
     return (
       <div className="RecipeCard__more">
+        {this.props.recipe.ingredients}
+        {/* {ingredients} */}
+        <hr/>
         {this.props.recipe.directions}
-        {this.props.recipe.story}
       </div>
     );
   }
@@ -132,7 +183,8 @@ class RecipeCard extends React.Component {
     const { recipe } = this.props;
     return (
       <div className="RecipeCard__title">
-        <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>{" "}
+        {/* <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>{" "} */}
+        {recipe.title}
       </div>
     );
   }
